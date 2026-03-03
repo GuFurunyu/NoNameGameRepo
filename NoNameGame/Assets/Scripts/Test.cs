d8c840677a9aa2aa54eb76b8ca6d3ed56ca3a6c8
Gomrade <3305405938@qq.com>
2026-03-03 23:33:24 +0800

    AddAnEmptyScriptToTest

---
+++ NoNameGame/Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[DefaultExecutionOrder((int)ScriptsExecutionOrder.ExecutionOrder.test)]
+public class Test : MonoBehaviour
+{
+    Constants CONS;
+    Variables VARS;
+    ScriptsExecutionController SEC;
+
+    GameObject gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+
+        CONS = gameManager.GetComponent<Constants>();
+        VARS = gameManager.GetComponent<Variables>();
+        SEC = gameManager.GetComponent<ScriptsExecutionController>();
+    }
+
+    //void Update()
+    //{
+    //    
+    //}
+}
